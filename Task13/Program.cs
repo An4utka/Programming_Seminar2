a19682c3180f3afe0718894e513e00e5db59960c
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-18 14:19:36 +0300

    Task 13 done

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter your number:");
+int a = int.Parse(Console.ReadLine()!);
+if (a > 99)
+{
+    string str = a.ToString();
+    Console.Write(str[2]);
+}
+else
+{
+    Console.Write("Sorry, there is no third figure here");
+}
