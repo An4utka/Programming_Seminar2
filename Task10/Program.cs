7790ccd51c52429616b00e0ad7e98cbaa5f92b22
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-18 14:21:53 +0300

    Task 10 done

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine("Enter 3-digit number:");
+int a = int.Parse(Console.ReadLine()!);
+int b = a/10;
+int c = a/100;
+int middle = b - c*10;
+Console.Write("Middle figure of the number:");
+Console.WriteLine(middle);
